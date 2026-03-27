4258317819117917f890e072ab10bea54655e493
alessandro210392-hash <alessandro210392@gmail.com>
2026-03-27 15:52:27 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,20 +7,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// percorso sicuro db
 const dbPath = path.join(__dirname, 'db.json');
-
-let db = { schools: [], users: [], questions: [] };
-
-try {
-  const data = fs.readFileSync(dbPath);
-  db = JSON.parse(data);
-} catch (err) {
-  console.log('Errore lettura db.json:', err);
-}
+let db = JSON.parse(fs.readFileSync(dbPath));
 
 app.get('/', (req, res) => {
-  res.send('Server funziona!');
+  res.send('Server funzionante');
 });
 
 // LOGIN
@@ -28,7 +19,7 @@ app.post('/login', (req, res) => {
   const { code, name } = req.body;
 
   const school = db.schools.find(s => s.code === code);
-  if (!school) return res.status(404).send('Scuola non trovata');
+  if (!school) return res.status(404).send('Codice non valido');
 
   let user = db.users.find(u => u.name === name);
 
@@ -44,16 +35,22 @@ app.post('/login', (req, res) => {
 app.get('/exam/:license', (req, res) => {
   const license = req.params.license;
 
-  const questions = db.questions.filter(q => q.license === license);
+  const config = {
+    B: { total: 30, maxErrors: 3, time: 1200 },
+    AM: { total: 30, maxErrors: 4, time: 1200 },
+    C: { total: 40, maxErrors: 4, time: 1600 },
+    D: { total: 40, maxErrors: 4, time: 1600 }
+  };
+
+  const questions = db.questions
+    .filter(q => q.license === license)
+    .sort(() => 0.5 - Math.random())
+    .slice(0, config[license]?.total || 30);
 
   res.json({
-    questions: questions.sort(() => 0.5 - Math.random()).slice(0, 30),
-    config: { maxErrors: 3 }
+    questions,
+    config: config[license] || { total: 30, maxErrors: 3, time: 1200 }
   });
 });
 
-const PORT = process.env.PORT || 3001;
-
-app.listen(PORT, () => {
-  console.log('Server attivo su ' + PORT);
-});
+app.listen(process.env.PORT || 3001);
